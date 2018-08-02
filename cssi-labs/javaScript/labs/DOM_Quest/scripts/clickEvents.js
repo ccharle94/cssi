907e60429c0c27473da1a9b89cdc61f76ee05635
ccharle94 <ccharle94@gmail.com>
2018-08-02 15:34:32 -0400

    all

--- cssi-labs/javaScript/labs/DOM_Quest/scripts/clickEvents.js
+++ cssi-labs/javaScript/labs/DOM_Quest/scripts/clickEvents.js
@@ -18,10 +18,6 @@ let box1 = document.getElementById('box1');
 let box2 = document.getElementById('box2');
 let box3 = document.getElementById('box3');
 
-
-
-
-
 box1.addEventListener('click', boxColor1);
 box2.addEventListener('click', boxColor2);
 box3.addEventListener('click', boxColor3);
